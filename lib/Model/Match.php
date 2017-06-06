d4243d6d76ffea854122939120afe0fc162065c0
Srnith <tsubasa4913@gmail.com>
2017-06-06 22:46:10 +0900

    [modify]message change bug fix

--- lib/Model/Match.php
+++ lib/Model/Match.php
@@ -96,7 +96,7 @@ class Match extends Base{
 		$bResult = true;
 		// 次のバッチ開始予定時間の1時間前取得
 		// TODO 月曜の00~5時の試合の処理も入れる必要あり、とりあえずは暫定対応
-		$nextExecuteBatchDate = date( 'Y-m-d 5:00:00', strtotime( $this->match_date . " next Monday") );
+		$nextExecuteBatchDate = date('Y-m-d H:i:s', strtotime( $oMatch->match_date . " + 4 hour" ) );
 		
 		if( date( 'Y-m-d H:i:s' ) > $nextExecuteBatchDate ){
 			$bResult = false;
