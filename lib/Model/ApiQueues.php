fac7266716eb8ed2a56438d8ecd28233d852af29
ygnizer <imifuwwwuhawwwokwww@gmail.com>
2017-09-10 10:50:34 +0900

    refs #20170910_func_batch_update_user ※カラム名間違いなおし；；

--- lib/Model/ApiQueues.php
+++ lib/Model/ApiQueues.php
@@ -7,7 +7,7 @@ class ApiQueues extends Base{
 	
 	// カラム
 	const DATA	= [
-		"member_id"			=> [ "type" => "int"		, "min" => 1	,"max" => 2147483647	, "required" => true	, "null" => false	],
+		"id"				=> [ "type" => "int"		, "min" => 1	,"max" => 2147483647	, "required" => true	, "null" => false	],
 		"action"			=> [ "type" => "int"		, "min" => 0	,"max" => 2147483647	, "required" => true	, "null" => false	],
 		"state"				=> [ "type" => "int"		, "min" => 0	,"max" => 127			, "required" => true	, "null" => false	],
 		"priority"			=> [ "type" => "int"		, "min" => -2147483648,"max"=>2147483647, "required" => true	, "null" => false	],
