5dd2a7d9a0bbd6bf7439dea542475d186e548df6
root <root@localhost.localdomain>
2018-10-28 03:16:13 +0900

    feat: 初期データ

--- batch/lns_batch/database/seeds/DatabaseSeeder.php
+++ batch/lns_batch/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(LolSeasonsTableSeeder::class);
+        $this->call(LeaguesTableSeeder::class);
     }
 }
