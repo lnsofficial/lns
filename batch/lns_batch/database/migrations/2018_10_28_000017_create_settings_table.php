cd65ab6c1aff6f87620e1fe7ec21db57039c7296
ygnizer <imifuwwwuhawwwokwww@gmail.com>
2018-10-28 18:50:24 +0900

    refactor: settingsテーブル追加

---
+++ batch/lns_batch/database/migrations/2018_10_28_000017_create_settings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    // ここ変える感じで。
+    private $tablename = 'settings';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if( !Schema::hasTable( $this->tablename ) )
+        {
+            Schema::create($this->tablename, function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->string('value');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists( $this->tablename );
+    }
+}
--- batch/lns_batch/database/seeds/DatabaseSeeder.php
+++ batch/lns_batch/database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(LolSeasonsTableSeeder::class);
         $this->call(LeaguesTableSeeder::class);
+        $this->call(SettingsTableSeeder::class);
     }
 }
